f01d2eac1964ba9a0fac0b89d0ba53b08f38cc93
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-06-23 13:15:30 +0530

    just created basic schema or model for database

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const { default: mongoose, mongo, models } = require('mongoose');
 const app = express();
 const path = require('path');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=>{
+    console.log("Database connected");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
