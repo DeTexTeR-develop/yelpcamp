fba3867c39f3e2bab8ba3291719dce0dabaa71ab
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-07-25 20:59:39 +0530

    set route for registering user

--- routers/users.js
+++ routers/users.js
@@ -6,5 +6,13 @@ const User = require('../models/user');
 router.get('/register', (req, res) => {
     res.render('Users/register');
 });
+router.post('/register', async(req, res) => {
+    const {  email , username , password } = req.body;
+    const user = new User({email, username});
+    const newUser =  await User.register(user, password);
+    console.log(newUser);
+    req.flash('Welcome to Yelpcamp');
+    res.redirect('/campgrounds')
+})
 
 module.exports = router;
