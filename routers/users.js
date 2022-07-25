d2707c37fc8767be393798e0d16ed9bb6eeca53a
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-07-25 21:09:25 +0530

    flash for username already used

--- routers/users.js
+++ routers/users.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 
 router.get('/register', (req, res) => {
     res.render('Users/register');
 });
-router.post('/register', async(req, res) => {
-    const {  email , username , password } = req.body;
-    const user = new User({email, username});
-    const newUser =  await User.register(user, password);
-    console.log(newUser);
-    req.flash('Welcome to Yelpcamp');
-    res.redirect('/campgrounds')
-})
+router.post('/register', catchAsync(async(req, res) => {
+    try{
+        const {  email , username , password } = req.body;
+        const user = new User({email, username});
+        const newUser =  await User.register(user, password);
+        console.log(newUser);
+        req.flash('success','Welcome to Yelpcamp');
+        res.redirect('/campgrounds')
+    }
+    catch(e){
+        req.flash('error', e.message);
+        res.redirect('/User/register')
+    }
+}));
 
 module.exports = router;
