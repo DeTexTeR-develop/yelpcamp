edcdf3f55261f671a4082799d29caeba9f797514
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-07-29 21:38:58 +0530

    implemented cloudinary to store photos

--- routers/campground.js
+++ routers/campground.js
@@ -3,13 +3,25 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor ,validateCampground} = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const {storage} = require('../cloudinary')
+const upload = multer({storage});
 
 router.get('/home',campgrounds.home)
 router.get('/create',isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/')
 	.get(catchAsync(campgrounds.indexPage))
-	.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground))
+	// .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground))
+
+	// for uploading single file
+	// .post(upload.single('image'), (req, res) => {
+
+	//for multiple files
+	.post(upload.array('image'), (req, res) => {
+		console.log(req.body , req.files);
+		res.send('it worked');
+	})
 
 router.route('/:id')
 	.get(catchAsync(campgrounds.showCampground))
