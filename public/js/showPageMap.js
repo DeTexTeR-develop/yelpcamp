a4e751e1cc82b4bf1f7df5c5d0de2a2df41ddc2d
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2022-08-01 18:52:58 +0530

    added cluster map

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -13,4 +13,10 @@ mapboxgl.accessToken = mapTokken;
         });
 new mapboxgl.Marker()
 .setLngLat(campground.geometry.coordinates)
+.setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+        .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+)
 .addTo(map)
